583609c69452d6b295ab4621394456373140bb8e
cagridogan08 <cagridogan08@gmail.com>
2022-08-11 10:03:01 +0300

    Exeption Handled & Event Denemeleri

--- BlazorGiris/Data/Expense.cs
+++ BlazorGiris/Data/Expense.cs
@@ -3,28 +3,18 @@
     public class Expense
     {
         public int id {  get; set; }
-        public string? title { get; set; }
+        public string? Title { get; set; }
         public double amount { get; set; }
         public string ? description { get; set; }
 
         public List<Expense> _expenseList= new List<Expense>();
 
 
-        public Expense(string Title,double _amount,string _description)
+        public Expense(string _title,double _amount,string _description)
         {
-            this.title = Title;
+            this.Title = _title;
             this.amount = _amount;
             this.description = _description;
-        }
-
-        public void FillList()
-        {
-            for(int i = 0; i < 6; i++)
-            {
-                Expense _first = new Expense("Test"+i, 15.49+i, "Blazor Model Denemesi "+i);
-                _first.id = i;
-                _expenseList.Add(_first);
-            }
         }
         public Expense GetItemById(int _id)
         {
